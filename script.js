ecda7d6916255c52f36a093b7a2b1b265e71f8b7
Clarice Nascimento <claricestfy@gmail.com>
2024-02-05 20:47:22 -0300

    :sparkles: feat: Criando o método de evento preventDefault()

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    checkInputs();
+});
+
